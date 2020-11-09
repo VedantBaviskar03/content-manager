b7429ede85d1ad6afbe2dd643e6c28494589019b
Deepjyoti Barman <deep.barman30@gmail.com>
2020-11-09 12:44:09 +0530

    Some quality of life patch for the input

--- content-frontend/src/components/Input.vue
+++ content-frontend/src/components/Input.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="input-new-entry">
         <div class="input-container">
-            Name <input v-model="enteredName" type="text" id="name">
-            Username <input v-model="enteredUser" type="text" id="username">
-            Email <input v-model="enteredMail" type="text" id="email">
-            URL <input v-model="enteredUrl" type="text" id="url">
-            Tags <input v-model="enteredTags" type="text" id="tags">
+            Name <input v-model="data.name" type="text" id="name">
+            Username <input v-model="data.username" type="text" id="username">
+            Email <input v-model="data.email" type="text" id="email">
+            URL <input v-model="data.url" type="text" id="url">
+            Tags <input v-model="data.tags" type="text" id="tags">
         </div>
         <button @click="enterContent">Add content</button>
     </div>
@@ -17,16 +17,23 @@ export default {
     data() {
         return {
             endpoint: "http://localhost:5000/",
-            enteredName: "",
-            enteredUser: "",
-            enteredMail: "",
-            enteredUrl: "",
-            enteredTags: ""
+            data: {
+                name: "",
+                username: "",
+                email: "",
+                url: "",
+                tags: ""
+            }
         }
     },
     methods: {
         emitEntered: function() {
             this.$emit("entered");
+            
+            // Clear all the data in the data object
+            for (let key in this.data) {
+                this.data[key] = ""
+            }
         },
         enterContent: async function() {
             await fetch(this.endpoint, {
@@ -34,13 +41,7 @@ export default {
                 headers: {
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify({
-                    "name": this.enteredName,
-                    "username": this.enteredUser,
-                    "email": this.enteredMail,
-                    "url": this.enteredUrl,
-                    "tags": this.enteredTags, 
-                })
+                body: JSON.stringify(this.data)
             });
             this.emitEntered();
         }
